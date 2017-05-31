4b55573877fa6c6ac035eb1eed6945499fb74361
jingedawang <wjg172184@163.com>
2017-05-31 21:09:45 +0800

    First version.

---
+++ cmake-build-debug/CMakeFiles/CeresCurveFitting.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CeresCurveFitting.dir/main.cpp.o"
+  "CeresCurveFitting.pdb"
+  "CeresCurveFitting"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CeresCurveFitting.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
